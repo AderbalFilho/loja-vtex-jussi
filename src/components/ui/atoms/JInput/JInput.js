1c458544864b76b6941dd42b132a75e6eafd67b1
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-13 16:12:38 -0300

    finish UI JInput component

--- src/components/ui/atoms/JInput/JInput.js
+++ src/components/ui/atoms/JInput/JInput.js
@@ -2,11 +2,24 @@ import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
+import JIcon from '../JIcon';
+
 import './JInput.scss';
 
-function JInput({ classes, internalValue, valueChange, ...rest }) {
+function JInput({
+  classes,
+  icon,
+  iconClicked,
+  internalValue,
+  valueChange,
+  ...rest
+}) {
   const [value, setValue] = useState('');
-  const className = classNames('j-input__input', classes);
+  const className = classNames(
+    'j-input__input',
+    icon && 'j-input__input--with-icon',
+    classes
+  );
 
   useEffect(() => {
     setValue(internalValue);
@@ -17,25 +30,40 @@ function JInput({ classes, internalValue, valueChange, ...rest }) {
     valueChange();
   }
 
+  function handleIconClick() {
+    iconClicked();
+  }
+
   return (
-    <div className=".j-input">
+    <div className="j-input">
       <input
         className={className}
         value={value}
         {...rest}
         onChange={handleChange}
-      ></input>
+      />
+      {icon && (
+        <JIcon
+          className="j-input__icon"
+          icon={icon}
+          onClick={handleIconClick}
+        />
+      )}
     </div>
   );
 }
 
 JInput.defaultProps = {
+  icon: '',
+  iconClicked: () => {},
   internalValue: '',
   valueChange: () => {},
 };
 
 JInput.propTypes = {
   classes: PropTypes.string,
+  icon: PropTypes.string,
+  iconClicked: PropTypes.func,
   internalValue: PropTypes.string,
   valueChange: PropTypes.func,
 };
--- src/components/ui/atoms/JInput/JInput.test.js
+++ src/components/ui/atoms/JInput/JInput.test.js
@@ -1,14 +1,63 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-// import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import JInput from './JInput';
 
 describe('JInput', () => {
   it('matches snapshot', () => {
-    const testRenderer = TestRenderer.create(<JInput />);
+    const testRenderer = TestRenderer.create(<JInput type="text" />);
     const jsonComponent = testRenderer.toJSON();
 
     expect(jsonComponent).toMatchSnapshot();
+  });
+
+  describe('with icon', () => {
+    it('matches snapshot', () => {
+      const testRenderer = TestRenderer.create(
+        <JInput type="text" icon="search" />
+      );
+      const jsonComponent = testRenderer.toJSON();
+
+      expect(jsonComponent).toMatchSnapshot();
+    });
+
+    it('fire event when icon clicked', () => {
+      const onClick = jest.fn();
+      const { getByAltText } = render(
+        <JInput type="text" icon="search" iconClicked={onClick} />
+      );
+
+      fireEvent.click(getByAltText(/buscar/i));
+
+      expect(onClick).toHaveBeenCalled();
+    });
+  });
+
+  it('changes internal value', () => {
+    const { container } = render(<JInput type="text" internalValue="Jüssi" />);
+    const input = container.querySelector('.j-input__input');
+
+    expect(input.value).toBe('Jüssi');
+  });
+
+  it('fire event when input text changed', () => {
+    const onClick = jest.fn();
+    const { getByPlaceholderText } = render(
+      <JInput type="text" placeholder="Buscar" valueChange={onClick} />
+    );
+
+    fireEvent.change(getByPlaceholderText(/Buscar/i), {
+      target: { value: 'Jüssi' },
+    });
+
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it('renders classes', () => {
+    const { container } = render(<JInput classes="search-input" type="text" />);
+    const input = container.querySelector('.j-input__input');
+
+    expect(input.classList).toContain('search-input');
   });
 });
