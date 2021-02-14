190956f8fb9c744699b1e5198fbe6cc95883ddaa
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-13 23:08:07 -0300

    create UI JButton

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,4 +1,5 @@
 // Atoms
+import JButton from './atoms/JButton';
 import JIcon from './atoms/JIcon';
 import JInput from './atoms/JInput';
 import JLink from './atoms/JLink';
@@ -6,4 +7,4 @@ import JSection from './atoms/JSection';
 import JText from './atoms/JText';
 import JTitle from './atoms/JTitle';
 
-export { JIcon, JInput, JLink, JSection, JText, JTitle };
+export { JButton, JIcon, JInput, JLink, JSection, JText, JTitle };
