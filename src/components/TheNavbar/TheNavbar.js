c0c5608d8117b4d5b5577a2f8642f64dcefdb641
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-02-14 22:45:29 -0300

    add classes to The Navbar

--- src/components/TheNavbar/TheNavbar.js
+++ src/components/TheNavbar/TheNavbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
 import { JLink } from '@ui';
 
 import './TheNavbar.scss';
 
-function TheNavbar({ links }) {
+function TheNavbar({ classes, links }) {
+  const className = classNames('the-navbar', classes);
+
   return (
-    <nav className="the-navbar">
+    <nav className={className}>
       {links &&
         links.map((link) => (
           <JLink
@@ -25,6 +28,7 @@ function TheNavbar({ links }) {
 }
 
 TheNavbar.propTypes = {
+  classes: PropTypes.string,
   links: PropTypes.arrayOf(
     PropTypes.shape({
       url: PropTypes.string,
